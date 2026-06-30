3fb71f6d45097c3c10d2d7de0f0c1377355b070b
Faisal Indrawan <isalindrawan@outlook.com>
2026-06-30 16:12:21 +0700

    optimization

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,13 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import icon from 'astro-icon';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://isalindrawan.netlify.app',
   vite: {
     plugins: [tailwindcss()],
   },
-  integrations: [icon()],
+  integrations: [icon(), sitemap()],
 });
